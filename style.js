499b96faa8aca4bc1d3b16cf6dd1aa839fe6b3f5
Joao Lucas B. Mendes <166837463+jlmeliodas@users.noreply.github.com>
2024-10-09 11:50:07 -0300

    velocidade de digitação

---
+++ style.js
@@ -0,0 +1,45 @@
+let frases = [
+    "O rato roeu a roupa do rei de Roma.",
+    "A ligeira raposa marrom saltou sobre o cão indolente.",
+    "O rapido rapaz marrom saltou sobre a raposa preguiçosa.",
+    "Trite tigre, triste tigre, que tristes tigres são os tigres tristes.",
+    "Se voce for um bom menino, ganhara um doce.",
+];
+
+let texto = document.querySelector("textarea")
+let tempo = document.querySelector("span")
+let frase = document.querySelector(".frase")
+let botaoGerar = document.querySelector(".gerarFrase")
+let botaoLimpar = document.querySelector(".limpar")
+let inicioTempo;
+let finalTempo;
+
+function escolherFrase() {
+    let index = Math.floor(Math.random()* frases.length)
+    frase.textContent = frases[index]
+}
+function iniciarContagem() {
+    if(!inicioTempo) {
+        inicioTempo = new Date().getTime()
+    }
+}
+function pararContagem(){
+    if(inicioTempo) {
+        finalTempo = new Date().getTime()
+        let tempoDigitacao = (finalTempo - inicioTempo) / 1000
+        tempo.textContent = tempoDigitacao.toFixed(2)
+        inicioTempo = null
+    }
+}
+function limpar() {
+    if(texto != "" && tempo !== 0) {
+       texto.value = ""
+       tempo.textContent = 0
+    }
+}
+
+escolherFrase()
+texto.addEventListener("keyup",iniciarContagem)
+texto.addEventListener("blur", pararContagem)
+botaoGerar.addEventListener("click",escolherFrase)
+botaoLimpar.addEventListener("click", limpar)
